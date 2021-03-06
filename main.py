2a9418a1adcf043cdc7ebdf90c5dfff79bcf84d4
Mounir <mounirvillaverde@gmail.com>
2021-03-06 18:55:29 +0100

    clean-up

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
- 
-app = Flask(__name__)
+from website import create_app
 
-
-@app.route("/login")
-def login():
-	return render_template("login.html")
-	
-@app.route("/signup")
-def signup():
-	return render_template("signup.html")
-
-@app.route("/home")
-def home():
-	return render_template("home.html", name=="khalil")
-
-@app.route("/")
-def begin():
-	return redirect("login")
+app = create_app()
 	
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
