4298926810bead9a5651c01d74833cca18ca513c
koc99 <Khalil.OualdChaib@student.uantwerpen.be>
2021-04-20 02:40:48 +0200

    dataset add

--- src/static/checked.js
+++ src/static/checked.js
@@ -25,3 +25,33 @@ function checker() {
         readonly()
     }
  }
+
+ function checker2(){
+    if (document.getElementById("checkbox").checked){
+        document.getElementById("upload_files").style.display = "block";
+        document.getElementById("add").style.display = "block";
+        document.getElementById("delete_but").style.display = "block";
+    }
+    else{
+        document.getElementById("upload_files").style.display = "none";
+        document.getElementById("add").style.display = "none";
+        document.getElementById("delete_but").style.display = "none";
+    }
+ }
+
+function add_upload(){
+    var itm = document.getElementById("meta-data_upload");
+    var cln = itm.cloneNode(true);
+    document.getElementById("upload_files").appendChild(cln);
+}
+
+function delete_upload(){
+    var list = document.getElementById("upload_files")
+    if(list.childNodes.length > 3){
+        var itm = document.getElementById("meta-data_upload")
+        itm.remove()
+        alert("lololo")
+    }
+    
+    
+}
